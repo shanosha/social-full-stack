c40186e25a183487b1c98763bdc2e9b389f300fb
Shanosha <wootenm1@gmail.com>
2026-03-26 16:59:29 -0400

    bugfix/fixed CLIENT_ORIGIN environment variable support

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN, 'http://localhost:5173' }))
+app.use(cors({ origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173'] }))
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
